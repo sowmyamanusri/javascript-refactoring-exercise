ee25da829dfefada86cefc31645aaaa2b5b1c43a
sowmyamanusri <79841813+sowmyamanusri@users.noreply.github.com>
2022-03-01 08:42:48 +0000

    Update transaction_processor.js
    Changed the Error throw  statement

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,8 +1,8 @@
 function processTransactions(transActions) {
-    (transActions = true) => {
-        throw new Error("Undefined collection of transactions");
-    }
-    let txCount = {}
+    if (!transActions) {
+    throw new Error("Undefined collection of transactions");
+  }
+    const txCount = {}
     transActions.map(item => txCount[item] ? txCount[item] += 1 : txCount[item] = 1);
     return Object.keys(txCount).sort((a, b) => txCount[b] - txCount[a] || a > b || -(a < b)).map(item => `${item} ${txCount[item]}`)
 }
