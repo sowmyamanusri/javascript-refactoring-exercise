97755fdbfe87f934c5096bd5334637a5fe0fa008
sowmyamanusri <79841813+sowmyamanusri@users.noreply.github.com>
2022-03-02 19:08:08 +0000

    Refactored the code in  transaction exercise

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,11 +1,15 @@
 function processTransactions(transActions) {
     if (!transActions) {
-    throw new Error("Undefined collection of transactions");
-  }
+        throw new Error(" undefined array of transactions");
+    }
     const txCount = {}
     transActions.forEach(item => txCount[item] ? txCount[item] += 1 : txCount[item] = 1);
-    return Object.keys(txCount).sort((a, b) => txCount[b] - txCount[a] || a > b || -(a < b)).map(item => `${item} ${txCount[item]}`)
+    return Object.keys(txCount).sort((itemOne, itemTwo) =>
+            txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo))
+        .map(item => `${item} ${txCount[item]}`)
 }
 
-
 module.exports = processTransactions;
+//['notebook', 'notebook', 'mouse', 'keyboard', 'mouse']
+//txCount = {"notebook" :2,"mouse" :2,"keyboard" :1};
+//[mouse 2, notebook 2, keyboard 1]
