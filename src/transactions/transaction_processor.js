9b1fe324c3f6f8f40d81f6cb15825f26179081e6
sowmyamanusri <79841813+sowmyamanusri@users.noreply.github.com>
2022-03-02 18:57:02 +0000

    Update transaction_processor.js
    Change map method to forEach method

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -3,7 +3,7 @@ function processTransactions(transActions) {
     throw new Error("Undefined collection of transactions");
   }
     const txCount = {}
-    transActions.map(item => txCount[item] ? txCount[item] += 1 : txCount[item] = 1);
+    transActions.forEach(item => txCount[item] ? txCount[item] += 1 : txCount[item] = 1);
     return Object.keys(txCount).sort((a, b) => txCount[b] - txCount[a] || a > b || -(a < b)).map(item => `${item} ${txCount[item]}`)
 }
 
